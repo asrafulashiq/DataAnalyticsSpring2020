30bbc77494b5c8f9709b3551fac786f544d17200
asrafulashiq <asrafulashiq@gmail.com>
2020-01-30 15:40:51 -0500

    renamed lab2 to lab1_part2

--- lab1_part2/ex_ggplot.R
+++ lab1_part2/ex_ggplot.R
@@ -2,3 +2,7 @@ library(ggplot2)
 ggplot(data=mtcars, mapping = aes(x=wt, y=mpg)) + geom_point() + geom_smooth()
 
 ggplot(data=mtcars, aes(x=factor(cyl))) + geom_bar()
+
+ggplot(data=mtcars, aes(x=mpg)) + geom_histogram(binwidth = 1)
+
+ggplot(data=ToothGrowth, aes(x=supp, y=len)) + geom_boxplot()
